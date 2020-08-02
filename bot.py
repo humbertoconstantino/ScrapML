4a4e3272ebfed7e3bb963163a731a291c42145e6
humbertoconstantino <humbertoconstantino18@gmail.com>
2020-08-01 23:56:23 -0300

    a

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ import os
 #oi
 product = os.environ.get('PRODUTO')
 chrome_options = Options()
-chrome_options.binary_location = os.environ.get('GOOGLE_CHROME_BIN2')
+chrome_options.binary_location = os.environ.get('GOOGLE_CHROME_BIN')
 caminho_chromedriver = os.environ.get('CHROMEDRIVER_PATH')
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--disable-dev-shm-usage')
